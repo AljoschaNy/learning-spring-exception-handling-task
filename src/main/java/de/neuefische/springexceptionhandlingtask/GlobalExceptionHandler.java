be01d4b841f9c1bb5f21a528817e0a212504278f
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-19 11:08:07 +0200

    Create GlobalExceptionHandler and add ExceptionHandler, remove ExceptionHandler from controller classes

---
+++ src/main/java/de/neuefische/springexceptionhandlingtask/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package de.neuefische.springexceptionhandlingtask;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    public ErrorMessage handleIllegalArgumentException(IllegalArgumentException exception) {
+        return new ErrorMessage(exception.getMessage());
+    }
+}
