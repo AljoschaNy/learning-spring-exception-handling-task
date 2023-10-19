c2f4d29b994753b98b60f719c17f0ccbe9285d94
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-19 12:50:53 +0200

    Catch all NoSuchElementExceptions with GlobalExceptionHandler

--- src/main/java/de/neuefische/springexceptionhandlingtask/ErrorMessage.java
+++ src/main/java/de/neuefische/springexceptionhandlingtask/ErrorMessage.java
@@ -1,6 +1,10 @@
 package de.neuefische.springexceptionhandlingtask;
 
+import jdk.jfr.Timestamp;
+
+@Timestamp
 public record ErrorMessage(
         String errorMessage
 ) {
+
 }
--- src/main/java/de/neuefische/springexceptionhandlingtask/GlobalExceptionHandler.java
+++ src/main/java/de/neuefische/springexceptionhandlingtask/GlobalExceptionHandler.java
@@ -5,11 +5,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.NoSuchElementException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
     public ErrorMessage handleIllegalArgumentException(IllegalArgumentException exception) {
+        return new ErrorMessage(exception.getMessage());
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorMessage handleNoSuchElementException(NoSuchElementException exception) {
         return new ErrorMessage(exception.getMessage());
     }
 
